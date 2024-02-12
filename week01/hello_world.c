51401923686e79b10ccd153530b7de3a75bd70cb
Rifa Jamal <z5311190@ad.unsw.edu.au>
2024-02-12 18:41:21 +1100

    initial commit

---
+++ week01/hello_world.c
@@ -0,0 +1,28 @@
+// Basic Hello World program
+// Marc Chee, September 2020
+
+#include <stdio.h>
+
+int main (void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+
+    printf("~ ~\n"
+            "0 0\n"
+            " o \n"
+            "\\_/\n");
+    return 0;
+}
+/*
+~ ~
+0 0
+ o
+\_/
+*/
+// compile
+// dcc hello_world.c -o my_program
+// dcc -o my_program hello_world.c
+
+// run program - ./my_program
